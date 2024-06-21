a30f3d7445735623f8e6fa46345379f561206778
Juraj Kmotorka <jurajkmo@gmail.com>
2024-06-21 10:09:42 +0200

    product list

--- components/productsList/ProductImage.tsx
+++ components/productsList/ProductImage.tsx
@@ -1,26 +1,33 @@
-import Image from 'next/legacy/image';
-import {getProductsListImg, IImagePartial} from '../../lib/imgs';
+import Image from "next/legacy/image";
+import { getProductsListImg, IImagePartial } from "../../lib/imgs";
 
-export default function ProductListImage({image, alt, maxSize = 200}: {image: IImagePartial, alt?: string, maxSize?: number}) {
-	const {src, blurSrc, width, height} = getProductsListImg(image, maxSize);
+export default function ProductListImage({
+	image,
+	alt,
+	maxSize = 350,
+}: {
+	image: IImagePartial;
+	alt?: string;
+	maxSize?: number;
+}) {
+	const { src, blurSrc, width, height } = getProductsListImg(image, maxSize);
 
 	return (
-		<div className={'img text-center'}>
-			{width && height
-				? <Image
+		<div className={"img text-center"}>
+			{width && height ? (
+				<Image
 					src={src}
 					width={width}
 					height={height}
-					placeholder='blur'
+					placeholder="blur"
 					blurDataURL={blurSrc}
 					quality={100}
-					itemProp='image'
+					itemProp="image"
 					alt={alt}
 				/>
-				: <img src={src}
-					alt={alt}
-					itemProp='image'
-				/>}
+			) : (
+				<img src={src} alt={alt} itemProp="image" />
+			)}
 		</div>
 	);
 }
