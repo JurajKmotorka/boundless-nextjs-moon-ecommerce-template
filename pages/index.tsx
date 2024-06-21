49286586cb6bd3ce2f43c7585ad9de81333f147e
Juraj Kmotorka <jurajkmo@gmail.com>
2024-06-21 10:28:58 +0200

    motto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
-import {IProduct} from 'boundless-api-client';
-import {GetServerSideProps, InferGetServerSidePropsType} from 'next';
-import ProductsList from '../components/ProductsList';
-import MainLayout from '../layouts/Main';
-import {apiClient} from '../lib/api';
-import {makeAllMenus} from '../lib/menu';
-import {IMenuItem} from '../@types/components';
-import bgImg from '../assets/cover-bg.jpeg';
-import bgPortraitImg from '../assets/cover-bg-portrait.jpg';
-import CoverTextInCenter from '../components/CoverTextInCenter';
-import ProductsSliderByQuery from '../components/ProductsSliderByQuery';
-import {IBasicSettings} from '../@types/settings';
+import { IProduct } from "boundless-api-client";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import ProductsList from "../components/ProductsList";
+import MainLayout from "../layouts/Main";
+import { apiClient } from "../lib/api";
+import { makeAllMenus } from "../lib/menu";
+import { IMenuItem } from "../@types/components";
+import bgImg from "../assets/cover-bg.jpeg";
+import bgPortraitImg from "../assets/cover-bg-portrait.jpg";
+import CoverTextInCenter from "../components/CoverTextInCenter";
+import ProductsSliderByQuery from "../components/ProductsSliderByQuery";
+import { IBasicSettings } from "../@types/settings";
 
-export default function IndexPage({products, mainMenu, footerMenu, basicSettings}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function IndexPage({
+	products,
+	mainMenu,
+	footerMenu,
+	basicSettings,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	return (
 		<MainLayout
 			mainMenu={mainMenu}
 			footerMenu={footerMenu}
-			classes={{layoutMain: 'pt-0'}}
+			classes={{ layoutMain: "pt-0" }}
 			basicSettings={basicSettings}
 		>
 			<CoverTextInCenter
@@ -24,42 +29,56 @@ export default function IndexPage({products, mainMenu, footerMenu, basicSettings
 				img={bgImg.src}
 				imgPortrait={bgPortraitImg.src}
 				content={{
-					intro: 'Muhammad Ali',
-					head: 'Impossible is nothing!',
-					subHead: 'Impossible is not a fact. It’s an opinion. Impossible is not a declaration. It’s a dare. Impossible is potential. Impossible is temporary.'
+					intro: "Laco Melisko",
+					head: "Voda kvape!",
+					subHead: "Sak vy doplatite.",
 				}}
 				shadow={{
 					opacity: 0.5,
-					backgroundColor: '#000'
+					backgroundColor: "#000",
 				}}
-				link={'https://google.com'}
+				link={"https://google.com"}
 			/>
-			<div className='container'>
-				<h1 className='page-heading page-heading_h1  page-heading_m-h1'>Boundless store</h1>
-				<ProductsList products={products} query={{}}/>
-				<h2 className='page-heading page-heading_h1  page-heading_m-h1'>Products carousel:</h2>
+			<div className="container">
+				<h1 className="page-heading page-heading_h1  page-heading_m-h1">
+					Reborned Store
+				</h1>
+				<ProductsList products={products} query={{}} />
+				<h2 className="page-heading page-heading_h1  page-heading_m-h1">
+					Products carousel:
+				</h2>
 				<ProductsSliderByQuery
-					query={{collection: ['main-page'], sort: 'in_collection'}}
-					wrapperClassName='page-block'
+					query={{ collection: ["main-page"], sort: "in_collection" }}
+					wrapperClassName="page-block"
 				/>
 			</div>
 		</MainLayout>
 	);
 }
 
-export const getServerSideProps: GetServerSideProps<IIndexPageProps> = async () => {
-	const categoryTree = await apiClient.catalog.getCategoryTree({menu: 'category'});
-	const {products} = await apiClient.catalog.getProducts({collection: ['main-page'], sort: 'in_collection'});
-	const basicSettings = await apiClient.system.fetchSettings(['system.locale', 'system.currency']) as IBasicSettings;
+export const getServerSideProps: GetServerSideProps<
+	IIndexPageProps
+> = async () => {
+	const categoryTree = await apiClient.catalog.getCategoryTree({
+		menu: "category",
+	});
+	const { products } = await apiClient.catalog.getProducts({
+		collection: ["main-page"],
+		sort: "in_collection",
+	});
+	const basicSettings = (await apiClient.system.fetchSettings([
+		"system.locale",
+		"system.currency",
+	])) as IBasicSettings;
 
-	const menus = makeAllMenus({categoryTree});
+	const menus = makeAllMenus({ categoryTree });
 
 	return {
 		props: {
 			products,
 			basicSettings,
-			...menus
-		}
+			...menus,
+		},
 	};
 };
 
